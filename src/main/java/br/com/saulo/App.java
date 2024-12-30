485f3c8c4af5267b0daa8a8ed37e8dd7724471cd
Saulo Vilela <saulovilelabarbosa@gmail.com>
2024-12-30 10:13:37 -0300

    Primeiro Commit para projeto selenium

---
+++ src/main/java/br/com/saulo/App.java
@@ -0,0 +1,13 @@
+package br.com.saulo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
